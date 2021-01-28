3b01246b9d1a39729c85fee3b6060c361a6c8f84
Ponzo <syozoshirakawa1208@gmail.com>
2021-01-28 10:41:41 +0000

    kadai-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_092805) do
+ActiveRecord::Schema.define(version: 2021_01_28_080113) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
